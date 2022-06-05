1b336665d01db8f82512f97a38aeed8389f8e9ca
vanminhle <vanminh.le.754@proton.me>
2022-06-05 19:39:23 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio id ipsum
+        vitae magni dolorem facere sint mollitia ratione, ea nobis quas commodi
+        iure minima eaque impedit omnis dolores corporis amet quos nostrum
+        inventore! Provident aliquam perferendis tempora voluptatum laudantium
+        excepturi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
